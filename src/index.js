55242e3ff5b1795dea8669a1c532aaf12fc352d5
Hira Muazzam <hira141998@gmail.com>
2021-08-15 10:47:00 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = 'https://twitter-api-backend1.herokuapp.com/';
 
 ReactDOM.render(
   <React.StrictMode>
